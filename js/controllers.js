3a79b608bf286882db433f833aabb88fcba8713c
TusharSachde <tushar@wohlig.com>
2016-05-17 12:50:54 +0530

    Remove Upper Case

--- js/controllers.js
+++ js/controllers.js
@@ -1,120 +1,119 @@
-angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap','ngAnimate', 'ngSanitize', 'angular-flexslider'])
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-flexslider'])
 
 .controller('HomeCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-  //Used to name the .html file
-
-  console.log("Testing Consoles");
-
-  $scope.template = TemplateService.changecontent("home");
-  $scope.menutitle = NavigationService.makeactive("Home");
-  TemplateService.title = $scope.menutitle;
-  $scope.navigation = NavigationService.getnav();
-})
-
-  .controller('AboutCtrl', function($scope, TemplateService, NavigationService, $timeout) {
     //Used to name the .html file
-
-    console.log("Testing Consoles");
-
-    $scope.template = TemplateService.changecontent("about-us");
-    $scope.menutitle = NavigationService.makeactive("ABOUT US");
+    $scope.template = TemplateService.changecontent("home");
+    $scope.menutitle = NavigationService.makeactive("Home");
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
-  })
 
-    .controller('ServicesCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-      //Used to name the .html file
+    $scope.mySlides = [{
+        image: 'img/slides/slide1.jpg',
+        title: 'Brigding',
+        text: 'lorem',
+        pagelink: 'about-us',
+        pagename: 'About Us'
+    }, {
+        image: 'img/slides/slide2.jpg',
+        title: '',
+        text: '',
+        pagelink: '',
+        pagename: ''
+    }, {
+        image: 'img/slides/slide3.jpg',
+        title: '',
+        text: '',
+        pagelink: '',
+        pagename: ''
+    }];
 
-      console.log("Testing Consoles");
+})
 
-      $scope.template = TemplateService.changecontent("services");
-      $scope.menutitle = NavigationService.makeactive("SERVICES");
-      TemplateService.title = $scope.menutitle;
-      $scope.navigation = NavigationService.getnav();
-    })
+.controller('AboutCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    //Used to name the .html file
+    $scope.template = TemplateService.changecontent("about-us");
+    $scope.menutitle = NavigationService.makeactive("About Us");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+})
 
-      .controller('AffiliatesCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-        //Used to name the .html file
+.controller('ServicesCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    //Used to name the .html file
+    $scope.template = TemplateService.changecontent("services");
+    $scope.menutitle = NavigationService.makeactive("SERVICES");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+})
 
-        console.log("Testing Consoles");
-
-        $scope.template = TemplateService.changecontent("affiliates");
-        $scope.menutitle = NavigationService.makeactive("AFFILIATES");
-        TemplateService.title = $scope.menutitle;
-        $scope.navigation = NavigationService.getnav();
-      })
-
-        .controller('ClientsCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-          //Used to name the .html file
-
-          console.log("Testing Consoles");
-
-          $scope.template = TemplateService.changecontent("clients");
-          $scope.menutitle = NavigationService.makeactive("CLIENTS");
-          TemplateService.title = $scope.menutitle;
-          $scope.navigation = NavigationService.getnav();
-        })
-
-        .controller('DownloadsCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-          //Used to name the .html file
-
-          console.log("Testing Consoles");
-
-          $scope.template = TemplateService.changecontent("downloads");
-          $scope.menutitle = NavigationService.makeactive("DOWNLOADS");
-          TemplateService.title = $scope.menutitle;
-          $scope.navigation = NavigationService.getnav();
-        })
-
-        .controller('ContactCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-          //Used to name the .html file
-
-          console.log("Testing Consoles");
-
-          $scope.template = TemplateService.changecontent("contact-us");
-          $scope.menutitle = NavigationService.makeactive("CONTACT US");
-          TemplateService.title = $scope.menutitle;
-          $scope.navigation = NavigationService.getnav();
-        })
+.controller('AffiliatesCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    //Used to name the .html file
 
 
 
-  $scope.mySlides = [
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
-  ];
+    $scope.template = TemplateService.changecontent("affiliates");
+    $scope.menutitle = NavigationService.makeactive("AFFILIATES");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+})
+
+.controller('ClientsCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    //Used to name the .html file
+
+
+
+    $scope.template = TemplateService.changecontent("clients");
+    $scope.menutitle = NavigationService.makeactive("CLIENTS");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+})
+
+.controller('DownloadsCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    //Used to name the .html file
+
+
+
+    $scope.template = TemplateService.changecontent("downloads");
+    $scope.menutitle = NavigationService.makeactive("DOWNLOADS");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+})
+
+.controller('ContactCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    //Used to name the .html file
+
+
+
+    $scope.template = TemplateService.changecontent("contact-us");
+    $scope.menutitle = NavigationService.makeactive("CONTACT US");
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
 })
 
 .controller('headerctrl', function($scope, TemplateService) {
-  $scope.template = TemplateService;
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-    $(window).scrollTop(0);
-  });
+    $scope.template = TemplateService;
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        $(window).scrollTop(0);
+    });
 })
 
-.controller('languageCtrl', function($scope, TemplateService,$translate,$rootScope) {
+.controller('languageCtrl', function($scope, TemplateService, $translate, $rootScope) {
 
     $scope.changeLanguage = function() {
-      console.log("Language CLicked");
+        console.log("Language CLicked");
 
-      if(!$.jStorage.get("language")){
-        $translate.use("hi");
-        $.jStorage.set("language","hi");
-      }
-      else {
-        if($.jStorage.get("language") == "en")
-        {
-          $translate.use("hi");
-          $.jStorage.set("language","hi");
+        if (!$.jStorage.get("language")) {
+            $translate.use("hi");
+            $.jStorage.set("language", "hi");
+        } else {
+            if ($.jStorage.get("language") == "en") {
+                $translate.use("hi");
+                $.jStorage.set("language", "hi");
+            } else {
+                $translate.use("en");
+                $.jStorage.set("language", "en");
+            }
         }
-        else {
-          $translate.use("en");
-          $.jStorage.set("language","en");
-        }
-      }
-    //  $rootScope.$apply();
+        //  $rootScope.$apply();
     };
 
 
--- js/navigation.js
+++ js/navigation.js
@@ -1,77 +1,73 @@
 var adminURL = "";
-if(isproduction)
-{
-  adminURL =  "http://www.wohlig.co.in/demo/index.php";
-}
-else {
-  adminURL = "http://localhost/demo/index.php";
+if (isproduction) {
+    adminURL = "http://www.wohlig.co.in/demo/index.php";
+} else {
+    adminURL = "http://localhost/demo/index.php";
 }
 
 var navigationservice = angular.module('navigationservice', [])
 
 .factory('NavigationService', function() {
-  var navigation = [{
-    name: "Home",
-    classis: "active",
-    anchor: "home",
-    subnav: [{
-      name: "Subnav1",
-      classis: "active",
-      anchor: "home"
-    }]
-  }];
+            var navigation = [{
+                    name: "Home",
+                    classis: "active",
+                    anchor: "home",
+                    subnav: [{
+                        name: "Subnav1",
+                        classis: "active",
+                        anchor: "home"
+                    }]
+                }, {
+                    name: "ABOUT US",
+                    classis: "active",
+                    anchor: "about-us",
+                    subnav: []
+                }, {
+                    name: "SERVICES",
+                    classis: "active",
+                    anchor: "services",
+                    subnav: []
+                }, {
+                    name: "AFFILIATES",
+                    classis: "active",
+                    anchor: "affiliates",
+                    subnav: []
+                }, {
+                    name: "CAREERS",
+                    classis: "active",
+                    anchor: "careers",
+                    subnav: []
+                }, {
+                    name: "CLIENTS",
+                    classis: "active",
+                    anchor: "clients",
+                    subnav: []
+                }, {
+                    name: "DOWNLOADS",
+                    classis: "active",
+                    anchor: "downloads",
+                    subnav: []
+                }, {
+                    name: "CONTACT US",
+                    classis: "active",
+                    anchor: "contact-us",
+                    subnav: []
+                }];
 
-  {
-  name: "ABOUT US",
-  classis: "active",
-  anchor: "about-us",
-  subnav: []
-},{
-  name: "SERVICES",
-  classis: "active",
-  anchor: "services",
-  subnav: []
-},{
-  name: "AFFILIATES",
-  classis: "active",
-  anchor: "affiliates",
-  subnav: []
-},{
-  name: "CAREERS",
-  classis: "active",
-  anchor: "careers",
-  subnav: []
-},{
-  name: "CLIENTS",
-  classis: "active",
-  anchor: "clients",
-  subnav: []
-},{
-  name: "DOWNLOADS",
-  classis: "active",
-  anchor: "downloads",
-  subnav: []
-},{
-  name: "CONTACT US",
-  classis: "active",
-  anchor: "contact-us",
-  subnav: []
-},
+                return {
+                    getnav: function() {
+                        return navigation;
+                    },
+                    makeactive: function(menuname) {
+                        for (var i = 0; i < navigation.length; i++) {
+                            if (navigation[i].name == menuname) {
+                                navigation[i].classis = "active";
+                            } else {
+                                navigation[i].classis = "";
+                            }
+                        }
+                        return menuname;
+                    },
 
-  return {
-    getnav: function() {
-      return navigation;
-    },
-    makeactive: function(menuname) {
-      for (var i = 0; i < navigation.length; i++) {
-        if (navigation[i].name == menuname) {
-          navigation[i].classis = "active";
-        } else {
-          navigation[i].classis = "";
-        }
-      }
-      return menuname;
-    },
-
-  };
-});
+                };
+            });
