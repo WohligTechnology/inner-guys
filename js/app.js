f0340343f44029cd8933f317f6bf8fc599d33344
Chintan <chintan@wohlig.com>
2016-05-30 12:06:38 +0530

    changes for the uploads

--- js/app.js
+++ js/app.js
@@ -1,126 +1,126 @@
 // JavaScript Document
 var firstapp = angular.module('firstapp', [
-  'ui.router',
-  'phonecatControllers',
-  'templateservicemod',
-  'navigationservice',
-  'pascalprecht.translate'
+    'ui.router',
+    'phonecatControllers',
+    'templateservicemod',
+    'navigationservice',
+    'pascalprecht.translate'
 ]);
 
 firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
-  // for http request with session
-  $httpProvider.defaults.withCredentials = true;
-  $stateProvider
-    .state('home', {
-      url: "/",
-      templateUrl: "views/template.html",
-      controller: 'HomeCtrl'
+    // for http request with session
+    $httpProvider.defaults.withCredentials = true;
+    $stateProvider
+        .state('home', {
+            url: "/",
+            templateUrl: "views/template.html",
+            controller: 'HomeCtrl'
+        })
+
+    .state('about-us', {
+        url: "/about-us",
+        templateUrl: "views/template.html",
+        controller: 'AboutCtrl'
     })
 
-  .state('about-us', {
-    url: "/about-us",
-    templateUrl: "views/template.html",
-    controller: 'AboutCtrl'
-  })
+    .state('services', {
+        url: "/services",
+        templateUrl: "views/template.html",
+        controller: 'ServicesCtrl'
+    })
 
-  .state('services', {
-    url: "/services",
-    templateUrl: "views/template.html",
-    controller: 'ServicesCtrl'
-  })
+    .state('affiliates', {
+        url: "/affiliates",
+        templateUrl: "views/template.html",
+        controller: 'AffiliatesCtrl'
+    })
 
-  .state('affiliates', {
-    url: "/affiliates",
-    templateUrl: "views/template.html",
-    controller: 'AffiliatesCtrl'
-  })
+    .state('careers', {
+        url: "/careers",
+        templateUrl: "views/template.html",
+        controller: 'CareersCtrl'
+    })
 
-  .state('careers', {
-    url: "/careers",
-    templateUrl: "views/template.html",
-    controller: 'CareersCtrl'
-  })
-
-  .state('clients', {
-    url: "/clients",
-    templateUrl: "views/template.html",
-    controller: 'ClientsCtrl'
-  })
+    .state('clients', {
+        url: "/clients",
+        templateUrl: "views/template.html",
+        controller: 'ClientsCtrl'
+    })
 
 
-  // .state('downloads', {
-  //   url: "/downloads",
-  //   templateUrl: "views/template.html",
-  //   controller: 'DownloadsCtrl'
-  // })
+    // .state('downloads', {
+    //   url: "/downloads",
+    //   templateUrl: "views/template.html",
+    //   controller: 'DownloadsCtrl'
+    // })
 
-  .state('contact-us', {
-    url: "/contact-us",
-    templateUrl: "views/template.html",
-    controller: 'ContactCtrl'
-  });
+    .state('contact-us', {
+        url: "/contact-us",
+        templateUrl: "views/template.html",
+        controller: 'ContactCtrl'
+    });
 
 
-  $urlRouterProvider.otherwise("/");
-  $locationProvider.html5Mode(isproduction);
+    $urlRouterProvider.otherwise("/");
+    $locationProvider.html5Mode(isproduction);
 });
 
 
 firstapp.directive('img', function($compile, $parse) {
-  return {
-    restrict: 'E',
-    replace: false,
-    link: function($scope, element, attrs) {
-      var $element = $(element);
-      if (!attrs.noloading) {
-        $element.after("<img src='img/loading.gif' class='loading' />");
-        var $loading = $element.next(".loading");
-        $element.load(function() {
-          $loading.remove();
-          $(this).addClass("doneLoading");
-        });
-      } else {
-        $($element).addClass("doneLoading");
-      }
-    }
-  };
+    return {
+        restrict: 'E',
+        replace: false,
+        link: function($scope, element, attrs) {
+            var $element = $(element);
+            if (!attrs.noloading) {
+                $element.after("<img src='img/loading.gif' class='loading' />");
+                var $loading = $element.next(".loading");
+                $element.load(function() {
+                    $loading.remove();
+                    $(this).addClass("doneLoading");
+                });
+            } else {
+                $($element).addClass("doneLoading");
+            }
+        }
+    };
 });
 
 firstapp.directive('fancyboxBox', function($document) {
-  return {
-    restrict: 'EA',
-    replace: false,
-    link: function(scope, element, attr) {
-      var $element = $(element);
-      var target;
-      if (attr.rel) {
-        target = $("[rel='" + attr.rel + "']");
-      } else {
-        target = element;
-      }
+    return {
+        restrict: 'EA',
+        replace: false,
+        link: function(scope, element, attr) {
+            var $element = $(element);
+            var target;
+            if (attr.rel) {
+                target = $("[rel='" + attr.rel + "']");
+            } else {
+                target = element;
+            }
 
-      target.fancybox({
-        openEffect: 'fade',
-        closeEffect: 'fade',
-        closeBtn: true,
-        helpers: {
-          media: {}
+            target.fancybox({
+                openEffect: 'fade',
+                closeEffect: 'fade',
+                closeBtn: true,
+                helpers: {
+                    media: {}
+                }
+            });
         }
-      });
-    }
-  };
+    };
 });
 
 firstapp.filter('uploadpath', function() {
     return function(input, width, height, style) {
         var other = "";
-        if (width && width != "") {
+        if (width && width !== "") {
             other += "&width=" + width;
         }
-        if (height && height != "") {
+        if (height && height !== "") {
             other += "&height=" + height;
         }
-        if (style && style != "") {
+        if (style && style !== "") {
             other += "&style=" + style;
         }
         if (input) {
@@ -154,8 +154,8 @@ firstapp.directive('uploadImage', function($http) {
                 $scope.model = [];
             };
             $scope.upload = function(image) {
-              console.log(image);
-              console.log("File");
+                console.log(image);
+                console.log("File");
                 var Template = this;
                 image.hide = true;
                 var formData = new FormData();
@@ -191,31 +191,40 @@ firstapp.directive('imageonload', function() {
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
-          console.log("Loading should start now",attrs.imageonload);
-          setTimeout(function() {
-            scope.$apply(attrs.imageonload);
-          } , 500);
+            console.log("Loading should start now", attrs.imageonload);
+            scope.attr = attrs;
+            scope.$watch(
+                "attr.change",
+                function handleFooChange(newValue, oldValue) {
+                    setTimeout(function() {
+                        scope.$apply(attrs.imageonload);
+                    }, 500);
+
+                }
+            );
+
+
         }
     };
 });
 
 firstapp.config(function($translateProvider) {
-  $translateProvider.translations('en', LanguageEnglish);
-  $translateProvider.translations('hi', LanguageHindi);
-  $translateProvider.preferredLanguage('en');
+    $translateProvider.translations('en', LanguageEnglish);
+    $translateProvider.translations('hi', LanguageHindi);
+    $translateProvider.preferredLanguage('en');
 });
 firstapp.directive('autoHeight', function($compile, $parse) {
-  return {
-    restrict: 'EA',
-    replace: false,
-    link: function($scope, element, attrs) {
-      var $element = $(element);
-      var windowHeight = $(window).height();
-      var newheight= windowHeight- 300;
-      var addHeight = function() {
-        $element.css("min-height", newheight);
-      };
-      addHeight();
-    }
-  };
+    return {
+        restrict: 'EA',
+        replace: false,
+        link: function($scope, element, attrs) {
+            var $element = $(element);
+            var windowHeight = $(window).height();
+            var newheight = windowHeight - 300;
+            var addHeight = function() {
+                $element.css("min-height", newheight);
+            };
+            addHeight();
+        }
+    };
 });
