55be983a73a5ab9ce99326249ff8d32e6afebc2e
deshmukhsanket <sanket.deshmukh7206@gmail.com>
2018-12-20 20:10:25 +0530

    done

--- js/navigation.js
+++ js/navigation.js
@@ -5,104 +5,103 @@
 // }
 var baseUrl = "http://104.199.171.145/backend/";
 // var baseUrl = "http://192.168.0.118/innergizebackend/";
-// var baseUrl = "http://suksha.com/innergizebackend/";
 var adminURL = baseUrl + "index.php/json/";
 var imgurl = baseUrl + "uploads/";
 var uploadurl = adminURL + "imageUpload";
 
 var navigationservice = angular.module('navigationservice', [])
 
-.factory('NavigationService', function($http) {
-  var navigation = [{
-    name: "About Us",
-    classis: "active",
-    anchor: "about-us",
-    subnav: []
-  }, {
-    name: "Services",
-    classis: "active",
-    anchor: "services",
-    subnav: []
-  }, {
-    name: "Affiliates",
-    classis: "active",
-    anchor: "affiliates",
-    subnav: []
-  }, {
-    name: "Clients",
-    classis: "active",
-    anchor: "clients",
-    subnav: []
-  }, {
-    name: "Knowledge Centre",
-    classis: "active",
-    anchor: "knowledge-centre",
-    subnav: []
-  }, {
-    name: "Careers",
-    classis: "active",
-    anchor: "careers",
-    subnav: []
-  },{
-    name: "Contact Us",
-    classis: "active",
-    anchor: "contact-us",
-    subnav: []
-  }];
+  .factory('NavigationService', function ($http) {
+    var navigation = [{
+      name: "About Us",
+      classis: "active",
+      anchor: "about-us",
+      subnav: []
+    }, {
+      name: "Services",
+      classis: "active",
+      anchor: "services",
+      subnav: []
+    }, {
+      name: "Affiliates",
+      classis: "active",
+      anchor: "affiliates",
+      subnav: []
+    }, {
+      name: "Clients",
+      classis: "active",
+      anchor: "clients",
+      subnav: []
+    }, {
+      name: "Knowledge Centre",
+      classis: "active",
+      anchor: "knowledge-centre",
+      subnav: []
+    }, {
+      name: "Careers",
+      classis: "active",
+      anchor: "careers",
+      subnav: []
+    }, {
+      name: "Contact Us",
+      classis: "active",
+      anchor: "contact-us",
+      subnav: []
+    }];
 
-  return {
-    getnav: function() {
-      return navigation;
-    },
-    submitContact: function(formData, callback) {
-      console.log(formData);
-      $http({
-        url: adminURL + 'contactSubmit',
-        method: 'POST',
-        withCredentials: true,
-        data: formData
-      }).success(callback);
-    },
-    submitCareers: function(formData, callback) {
-      console.log(formData);
-      $http.post(adminURL + 'careersSubmit', formData).success(callback);
-    },
+    return {
+      getnav: function () {
+        return navigation;
+      },
+      submitContact: function (formData, callback) {
+        console.log(formData);
+        $http({
+          url: adminURL + 'contactSubmit',
+          method: 'POST',
+          withCredentials: true,
+          data: formData
+        }).then(callback);
+      },
+      submitCareers: function (formData, callback) {
+        console.log(formData);
+        $http.post(adminURL + 'careersSubmit', formData).then(callback);
+      },
 
 
-    getCategoryData: function(callback) {
-      $http.get(adminURL + 'getCategory').success(callback);
-    },
+      getCategoryData: function (callback) {
+        $http.get(adminURL + 'getCategory').then(callback);
+      },
 
-    getCategoryId: function(id, callback) {
-      $http.get(adminURL + 'getCategory?category=' + id).success(callback);
-    },
+      getCategoryId: function (id, callback) {
+        $http.get(adminURL + 'getCategory?category=' + id).then(callback);
+      },
 
-    // submitContact: function(formData, callback) {
-    //   console.log(formData);
-    //   $http({
-    //     url: adminURL + 'json/contactSubmit',
-    //     method: 'POST',
-    //     withCredentials: true,
-    //     data: {
-    //       "name": formData.name,
-    //       "company": formData.company,
-    //       "email": formData.email,
-    //       "phone": formData.phone,
-    //       "website": formData.website,
-    //       "comments": formData.comments,
-    //     }
-    //   }).success(callback);
-    // },
-    makeactive: function(menuname) {
-      for (var i = 0; i < navigation.length; i++) {
-        if (navigation[i].name == menuname) {
-          navigation[i].classis = "active";
-        } else {
-          navigation[i].classis = "";
+      // submitContact: function(formData, callback) {
+      //   console.log(formData);
+      //   $http({
+      //     url: adminURL + 'json/contactSubmit',
+      //     method: 'POST',
+      //     withCredentials: true,
+      //     data: {
+      //       "name": formData.name,
+      //       "company": formData.company,
+      //       "email": formData.email,
+      //       "phone": formData.phone,
+      //       "website": formData.website,
+      //       "comments": formData.comments,
+      //     }
+      //   }).success(callback);
+      // },
+      makeactive: function (menuname) {
+        for (var i = 0; i < navigation.length; i++) {
+          if (navigation[i].name == menuname) {
+            navigation[i].classis = "active";
+          } else {
+            navigation[i].classis = "";
+          }
         }
-      }
-      return menuname;
-    },
+        return menuname;
+      },
 
-  };
-});
+    };
+  });
